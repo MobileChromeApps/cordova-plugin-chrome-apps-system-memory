c9703d08b5074a5880d6cdece46bb082ad9b10b7
Jason Chase <jasonpchase@gmail.com>
2015-04-01 15:34:30 -0400

    Convert manual tests to use Cordova plugin test framework

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-registerManualTests('chrome.system.memory', function(rootEl, addButton) {
+exports.defineManualTests = function(rootEl, addButton) {
   addButton('Get Info', function() {
     chrome.system.memory.getInfo(function(info) {
       logger(JSON.stringify(info, null, 4));
     });
   });
-});
+};
